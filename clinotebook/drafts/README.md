71e619efafae80771d0a672c5ad0287db3251175
LYDERBUG <nidiastudioph@gmail.com>
2023-01-03 10:44:41 +0800

    added a basic prompt, currently nothig is functional

---
+++ clinotebook/drafts/README.md
@@ -0,0 +1,1 @@
+This is the folder of our code drafts before implementing to main file
--- clinotebook/src/main.cpp
+++ clinotebook/src/main.cpp
@@ -1,5 +1,26 @@
 #include<iostream>
+#include<fstream>
+#include<string>
 
+
+void prompt() {
+    std::string input;
+
+    //Anti Enter spamming
+    do {
+        std::cout << "NIDIA CLINotebook v1.0\n"
+            << "Read, Write notes !help for command information or help <command>\n\n"
+            << "$: ";
+        getline(std::cin, input); //Entering Enter (pun intended) will cause a blank input hence, the if statement below is true
+        if (input.empty()) {
+            system("cls"); //yea I know lol
+        }
+    } while (input.empty());
+    system("cls");
+}
 int main() {
-	return 0;
+    while(1){
+        prompt();
+    }
+    return 0;
 }
