7a397a70fa877d4dfe2e1ee101b5c1456cd8ece9
LYDERBUG <nidiastudioph@gmail.com>
2023-01-05 07:23:31 +0800

    implemented a better anti spam

--- clinotebook/src/main.cpp
+++ clinotebook/src/main.cpp
@@ -1,22 +1,43 @@
+//Command Line Interface based notepad created by @LYDERBUG
+/* symbol meaning
+    !  : help
+    <> : parameter
+    [] : input
+    $: : console
+*/
 #include<iostream>
 #include<fstream>
 #include<string>
 
-
+//int x = 0;
+void commandHelp() {
+    std::cout << "This is rust\n";
+    std::cin.get();
+}
 void prompt() {
     std::string input;
-
+    
     //Anti Enter spamming
     do {
-        std::cout << "NIDIA CLINotebook v1.0\n"
-            << "Read, Write notes !help for command information or help <command>\n\n"
+        system("cls");
+        std::cout
+            << "NIDIA CLINotebook v1.0\n"
+            << "Read, Write notes !help for all command information, or !help <command>\n\n"
             << "$: ";
         getline(std::cin, input); //Entering Enter (pun intended) will cause a blank input hence, the if statement below is true
-        if (input.empty()) {
-            system("cls"); //yea I know lol
+        if (!input.empty()) {
+            if (input == "!help") {
+                commandHelp();
+            }
+            else {
+                system("cls"); //yea I know lol
+                //continue;
+            }
         }
+
     } while (input.empty());
-    system("cls");
+
+    //system("cls");
 }
 int main() {
     while(1){
